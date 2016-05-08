1ce417f1378bfc6a175cd661dad53a651ad8749a
Mike Lenzen <lenzenmi@gmail.com>
2016-05-08 15:24:15 +0600

    pep8

--- peewee_versioned/test_migrate.py
+++ peewee_versioned/test_migrate.py
@@ -20,33 +20,38 @@ if database_url:
         migrator = PostgresqlMigrator.from_database(database)
     if database_url.startswith('sqlite'):
         migrator = SqliteMigrator.from_database(database)
-    
+
 else:
     database = SqliteDatabase(':memory:')
     migrator = SqliteMigrator.from_database(database)
-    
+
 introspector = Introspector.from_database(database)
 
 # Basic example class
+
+
 class BaseClass(VersionedModel):
+
     class Meta:
         database = database
-    
-    
+
+
 class Food(BaseClass):
     name = CharField(null=True)
     is_tasty = BooleanField()
-    
+
+
 class Chow(BaseClass):
     name = CharField(null=True)
     is_tasty = BooleanField()
-    
-    
+
+
 class Menu(BaseClass):
     name = CharField()
 
 
 class TestMigrations(unittest.TestCase):
+
     def setUp(self):
         Food.create_table()
         Menu.create_table()
@@ -58,42 +63,50 @@ class TestMigrations(unittest.TestCase):
             # Food does not exist after rename_table_test
             Chow.drop_table()
         Menu.drop_table()
-    
-    ### Helper Functions ###
-    
+
+    # Helper Functions
+
     def assertTableHasColumn(self, table, column, type_=None):
         models = introspector.generate_models()
-        self.assertTrue(table in models, "table '{}' should be in models".format(table))
-        self.assertTrue(column in models[table]._meta.fields, "column '{}' should be in fields".format(column))
+        self.assertTrue(
+            table in models, "table '{}' should be in models".format(table))
+        self.assertTrue(column in models[
+                        table]._meta.fields, "column '{}' should be in fields".format(column))
         if type_ is not None:
-            self.assertTrue(isinstance(models[table]._meta.fields[column], type_))
+            self.assertTrue(
+                isinstance(models[table]._meta.fields[column], type_))
 
     def assertTableDoesNotHaveColumn(self, table, column):
         models = introspector.generate_models()
-        self.assertTrue(table in models, "table '{}' should be in models".format(table))
-        self.assertFalse(column in models[table]._meta.fields, "column '{}' should be in fields".format(column))
+        self.assertTrue(
+            table in models, "table '{}' should be in models".format(table))
+        self.assertFalse(column in models[
+                         table]._meta.fields, "column '{}' should be in fields".format(column))
 
     def assertTableExists(self, table):
         models = introspector.generate_models()
-        self.assertTrue(table in models, "table '{}' should be in models".format(table))
-        
+        self.assertTrue(
+            table in models, "table '{}' should be in models".format(table))
+
     def assertTableDoesNotExist(self, table):
         models = introspector.generate_models()
-        self.assertFalse(table in models, "table '{}' should be in models".format(table))
-    
-    ### Tests ###
-    
+        self.assertFalse(
+            table in models, "table '{}' should be in models".format(table))
+
+    # Tests
+
     def test_add_column(self):
         another_column = CharField(null=True)
         migrate(migrator.add_column('food', 'another_column', another_column))
         self.assertTableHasColumn('food', 'another_column', CharField)
         self.assertTableHasColumn('foodversion', 'another_column', CharField)
-        
+
     def test_add_column_foreign_key(self):
         '''
         Versioned Models should not have foreign key references
         '''
-        another_column = ForeignKeyField(Menu, related_name='food', null=True, to_field=Menu.id)
+        another_column = ForeignKeyField(
+            Menu, related_name='food', null=True, to_field=Menu.id)
         migrate(migrator.add_column('food', 'another_column', another_column))
         self.assertTableHasColumn('food', 'another_column', ForeignKeyField)
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
@@ -102,32 +115,34 @@ class TestMigrations(unittest.TestCase):
         migrate(migrator.drop_column('food', 'is_tasty'))
         self.assertTableDoesNotHaveColumn('food', 'is_tasty')
         self.assertTableDoesNotHaveColumn('foodversion', 'is_tasty')
-        
+
     def test_drop_column_not_in_version(self):
-        another_column = ForeignKeyField(Menu, related_name='food', null=True, to_field=Menu.id)
+        another_column = ForeignKeyField(
+            Menu, related_name='food', null=True, to_field=Menu.id)
         migrate(migrator.add_column('food', 'another_column', another_column))
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
         migrate(migrator.drop_column('food', 'another_column'))
         self.assertTableDoesNotHaveColumn('food', 'another_column')
-        
+
     def test_rename_column(self):
         migrate(migrator.rename_column('food', 'is_tasty', 'was_tasty'))
         self.assertTableDoesNotHaveColumn('food', 'is_tasty')
         self.assertTableDoesNotHaveColumn('foodversion', 'is_tasty')
         self.assertTableHasColumn('food', 'was_tasty')
         self.assertTableHasColumn('foodversion', 'was_tasty')
-        
+
     def test_rename_column_not_in_version(self):
-        another_column = ForeignKeyField(Menu, related_name='food', null=True, to_field=Menu.id)
+        another_column = ForeignKeyField(
+            Menu, related_name='food', null=True, to_field=Menu.id)
         migrate(migrator.add_column('food', 'another_column', another_column))
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
-        
+
         migrate(migrator.rename_column('food', 'another_column', 'new_column'))
         self.assertTableDoesNotHaveColumn('food', 'another_column')
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
         self.assertTableHasColumn('food', 'new_column')
         self.assertTableDoesNotHaveColumn('foodversion', 'new_column')
-        
+
     def test_add_not_null(self):
         models = introspector.generate_models()
         food = models['food']
@@ -136,30 +151,30 @@ class TestMigrations(unittest.TestCase):
         self.assertTrue(foodversion.name.null)
 
         migrate(migrator.add_not_null('food', 'name'))
-        models = introspector.generate_models() 
+        models = introspector.generate_models()
         food = models['food']
         foodversion = models['foodversion']
         self.assertFalse(food.name.null)
         self.assertFalse(foodversion.name.null)
-        
+
     def test_add_not_null_not_in_version(self):
         another_column = CharField(null=True, default='')
         migrate(migrator.add_column('food', 'another_column', another_column))
-        
+
         # drop the field from the nested version
         migrator.drop_column('foodversion', 'another_column').run()
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
-        
+
         models = introspector.generate_models()
         food = models['food']
         self.assertTrue(food.another_column.null)
 
         migrate(migrator.add_not_null('food', 'another_column'))
-        models = introspector.generate_models() 
+        models = introspector.generate_models()
         food = models['food']
         self.assertFalse(food.another_column.null)
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
-        
+
     def test_drop_not_null(self):
         models = introspector.generate_models()
         food = models['food']
@@ -168,50 +183,50 @@ class TestMigrations(unittest.TestCase):
         self.assertFalse(foodversion.is_tasty.null)
 
         migrate(migrator.drop_not_null('food', 'is_tasty'))
-        models = introspector.generate_models() 
+        models = introspector.generate_models()
         food = models['food']
         foodversion = models['foodversion']
         self.assertTrue(food.is_tasty.null)
         self.assertTrue(foodversion.is_tasty.null)
-        
+
     def test_drop_not_null_not_in_version(self):
         another_column = CharField(default='')
         migrate(migrator.add_column('food', 'another_column', another_column))
-        
+
         # drop the field from the nested version
         migrator.drop_column('foodversion', 'another_column').run()
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
-        
+
         models = introspector.generate_models()
         food = models['food']
         self.assertFalse(food.another_column.null)
 
         migrate(migrator.drop_not_null('food', 'another_column'))
-        models = introspector.generate_models() 
+        models = introspector.generate_models()
         food = models['food']
         self.assertTrue(food.another_column.null)
         self.assertTableDoesNotHaveColumn('foodversion', 'another_column')
-    
+
     def test_rename_table(self):
         self.assertTableExists('food')
         self.assertTableExists('foodversion')
-        
+
         # Create some foods
         food_kwargs = ({'name': '1', 'is_tasty': False},
-                 {'name': '2', 'is_tasty': False},
-                 {'name': '3', 'is_tasty': False},
-                 {'name': '4', 'is_tasty': False})
-        
+                       {'name': '2', 'is_tasty': False},
+                       {'name': '3', 'is_tasty': False},
+                       {'name': '4', 'is_tasty': False})
+
         for kwargs in food_kwargs:
-            food = Food.create(**kwargs)
-            
+            Food.create(**kwargs)
+
         migrate(migrator.rename_table('food', 'chow'))
-        
+
         self.assertTableExists('chow')
         self.assertTableExists('chowversion')
         self.assertTableDoesNotExist('food')
         self.assertTableDoesNotExist('foodversion')
-        
+
         # check to make sure the versions still exist
         for kwargs in food_kwargs:
             chow = Chow.get(Chow.name == kwargs['name'])
@@ -220,23 +235,24 @@ class TestMigrations(unittest.TestCase):
             for key, value in kwargs.items():
                 self.assertEqual(value, getattr(chow, key))
                 self.assertEqual(value, getattr(version, key))
-                
+
     def test_add_index(self):
         migrate(migrator.add_index('food', ['name']))
         models = introspector.generate_models()
         self.assertTrue(models['food'].name.index)
         self.assertFalse(models['foodversion'].name.index)
-        
+
     def test_drop_index(self):
         # create an index
         migrate(migrator.add_index('food', ['name']))
         models = introspector.generate_models()
         self.assertTrue(models['food'].name.index)
-        
+
         # Drop the index
         migrate(migrator.drop_index('food', 'food_name'))
         models = introspector.generate_models()
         self.assertFalse(models['food'].name.index)
         self.assertFalse(models['foodversion'].name.index)
-            
-            
+
+if __name__ == '__main__':
+    unittest.main()
--- peewee_versioned/test_versioned.py
+++ peewee_versioned/test_versioned.py
@@ -11,13 +11,14 @@ from . import VersionedModel
 database_url = os.environ.get('DATABASE', None)
 if database_url:
     database = connect(database_url)
-    
+
 else:
     database = SqliteDatabase(':memory:')
 
 
 # Basic example class
 class BaseClass(VersionedModel):
+
     class Meta:
         database = database
 
@@ -46,7 +47,8 @@ class TestVersionedModel(unittest.TestCase):
         self.assertEqual(loaded_person, self.person)
         for key, value in self.person_kwargs.items():
             self.assertEqual(value, getattr(loaded_person, key))
-            self.assertEqual(getattr(self.person, key), getattr(loaded_person, key))
+            self.assertEqual(
+                getattr(self.person, key), getattr(loaded_person, key))
 
     def test_should_update_as_normal_model(self):
         self.person.is_relative = False  # toggle
@@ -67,7 +69,8 @@ class TestVersionedModel(unittest.TestCase):
         self.assertFalse(os.path.exists('peewee.db'))
 
     def test_should_create_nested_version_models(self):
-        self.assertTrue(hasattr(Person, '_VersionModel'), 'Should have a _version_model attribute')
+        self.assertTrue(
+            hasattr(Person, '_VersionModel'), 'Should have a _version_model attribute')
         self.assertTrue(issubclass(Person._VersionModel, Person))
 
     def test_should_not_infinitely_recurse_when_creating_nested_version_models(self):
@@ -89,7 +92,8 @@ class TestVersionedModel(unittest.TestCase):
 
     def test_deleteing_instance_should_create_new_version(self):
         original_versions = Person._VersionModel.select()
-        self.assertEqual(len(original_versions), 1, 'should begin with one version')
+        self.assertEqual(
+            len(original_versions), 1, 'should begin with one version')
         self.assertFalse(original_versions[0]._deleted)
 
         # delete the instance
@@ -118,7 +122,8 @@ class TestVersionedModel(unittest.TestCase):
 
     def test_update_should_create_new_version(self):
         original_versions = Person._VersionModel.select()
-        self.assertEqual(len(original_versions), 1, 'should begin with one version')
+        self.assertEqual(
+            len(original_versions), 1, 'should begin with one version')
         self.assertFalse(original_versions[0]._deleted)
 
         # modify the instance
@@ -142,12 +147,13 @@ class TestVersionedModel(unittest.TestCase):
                        .where(Person._VersionModel._valid_until.is_null(False))
                        )[0]
         for key, value in self.person_kwargs.items():
-                self.assertEqual(getattr(old_version, key), value)
+            self.assertEqual(getattr(old_version, key), value)
 
         # Check attributes of current version
         current_version = self.person._get_current_version()
         for key in self.person_kwargs.keys():
-                self.assertEqual(getattr(current_version, key), getattr(self.person, key))
+            self.assertEqual(
+                getattr(current_version, key), getattr(self.person, key))
 
     def test_create_new_record_with_save_should_create_version(self):
         new_person = Person()
@@ -165,7 +171,8 @@ class TestVersionedModel(unittest.TestCase):
         self.assertEqual(self.person.version_id, 1)
 
     def test_version_id_shold_increment(self):
-        test_versions_names = ((num, str(num)) for num in range(2, 100))  # (2, '2'), (3, '3')...
+        # (2, '2'), (3, '3')...
+        test_versions_names = ((num, str(num)) for num in range(2, 100))
         for version, name in test_versions_names:
             self.person.name = name
             self.person.save()
@@ -256,6 +263,7 @@ class Student(BaseClass):
 
 
 class TestRelations(unittest.TestCase):
+
     def setUp(self):
         School.create_table()
         Student.create_table()
@@ -279,10 +287,10 @@ class TestRelations(unittest.TestCase):
         self.student2.school = self.school
         self.student2.save()
 
-        self.assertEqual(len(self.school.students), 2, 'Should have 2 students')
+        self.assertEqual(
+            len(self.school.students), 2, 'Should have 2 students')
         self.assertEqual(self.student.school, self.school)
         self.assertEqual(self.student2.school, self.school)
-
 
 
 if __name__ == '__main__':
